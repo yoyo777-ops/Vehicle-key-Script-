23bbc58a025d3933123c1ea4882dfdf5d4311136
yoyo777-ops <dharshansm226@gmail.com>
2026-07-29 20:01:56 +0530

    Refactor vehicle lock state management

--- vehiclekey/server.lua
+++ vehiclekey/server.lua
@@ -1,51 +1,46 @@
 local charIdLogout = {}
 
-local function vehiclelights(vehicle)
-    CreateThread(function()
-        for _ = 0, 1 do
-            SetVehicleLights(vehicle, 2)
-            Citizen.Wait(250)
-            SetVehicleLights(vehicle, 0)
-            Citizen.Wait(250)
-            SetVehicleLights(vehicle, 2)
-            Citizen.Wait(250)
-            SetVehicleLights(vehicle, 0)
-            Wait(600)
-        end
-    end)
-end
-
-
 lib.callback.register('changevehiclelockstate', function(source, vehicleNetId)
     local vehicle = NetworkGetEntityFromNetworkId(vehicleNetId)
+
+    if not DoesEntityExist(vehicle) then
+        return
+    end
+
     local lockState = Entity(vehicle).state.lockState
+
     if lockState == nil then
         lockState = 2
         Entity(vehicle).state:set('lockState', lockState, true)
     end
+
     local player = Ox.GetPlayer(source).charId
+
     if charIdLogout[player] then
         return
     end
+
     local oxvehicle = Ox.GetVehicle(vehicle)
 
     if oxvehicle == nil then
         return 3, 'This vehicle is not owned by anyone'
     end
+
     local vehicleowner = oxvehicle.owner
 
     if lockState == 1 then
         if vehicleowner ~= player then
             return 3, 'You do not have the keys to this vehicle'
         end
-        vehiclelights(vehicle)
+
         Entity(vehicle).state:set('lockState', 2, true)
+
         return 1, 'Vehicle locked'
     elseif lockState == 2 then
         if vehicleowner ~= player then
             return 3, 'You do not have the keys to this vehicle'
         end
-        vehiclelights(vehicle)
+
         Entity(vehicle).state:set('lockState', 1, true)
 
         return 2, 'Vehicle unlocked'
